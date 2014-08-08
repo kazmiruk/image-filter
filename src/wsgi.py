548c0b72a8b389d971bcf71febc63a6d5039fa03
kazmiruk <kazmiruk@live.ru>
2014-08-08 17:28:29 +0800

    refs #1164: return 404 on IOError exception

--- src/wsgi.py
+++ src/wsgi.py
@@ -10,18 +10,21 @@ dictConfig(settings.LOGGING)
 def application(environ, start_response):
     """application which realize wsgi interface
     """
+    host = "{schema}://{host}".format(
+        schema=environ['wsgi.url_scheme'],
+        host=environ['HTTP_HOST']
+    )
+    path = environ['PATH_INFO']
+
+    logging.info("Query obtained with path {path}".format(
+        path=environ['PATH_INFO']
+    ))
+
     try:
-        host = "{schema}://{host}".format(
-            schema=environ['wsgi.url_scheme'],
-            host=environ['HTTP_HOST']
-        )
-        path = environ['PATH_INFO']
-
-        logging.info("Query obtained with path {path}".format(
-            path=environ['PATH_INFO']
-        ))
-
         return process(host, path, start_response)
+    except IOError, e:
+        start_response('404 Not Found', [])
+        return ["{host}{path} Not Found".format(host=host, path=path)]
     except Exception, e:
         logging.error(e)
         raise e
